0f02b4741c12f198324201c17c2956e9467081b4
levi <levi@ghtinc.com>
2018-01-29 17:09:07 +0800

    examine ego-ent files; iter uses up to two threads; rm iter bug

--- examime.sh
+++ examime.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 
-dataResource="yelp" #1m" #100k" #
+#dataResource="ego-net0" #"yelp" #1m" #"100k" #
 line=15
 
-for f in "100F" "200F" "300F" "400F"
+for dataResource in "ego-net0" "ego-net107" "ego-net1684" "ego-net1912" "ego-net3437" "ego-net348" "ego-net3980" "ego-net414" "ego-net686" "ego-net698"
 do
   echo ""
-  echo "$f"
-  echo "microF1"
-  grep "logistic regression done" -A ${line} report/${f}${dataResource}_* | grep "valid data microF1" | awk -F'==' '{print $2}'
-  echo "one"
-  grep "logistic regression done" -A ${line} report/${f}${dataResource}_* | grep "valid data oneError" | awk -F'==' '{print $2}'
-  echo "rl"
-  grep "logistic regression done" -A ${line} report/${f}${dataResource}_* | grep "valid data RL" | awk -F'==' '{print $2}'
-  echo "coverage"
-  grep "logistic regression done" -A ${line} report/${f}${dataResource}_* | grep "valid data coverage" | awk -F'==' '{print $2}'
-  echo "avg Prec"
-  grep "logistic regression done" -A ${line} report/${f}${dataResource}_* | grep "valid data avgPrec" | awk -F'==' '{print $2}'
-  echo "hamming"
-  grep "logistic regression done" -A ${line} report/${f}${dataResource}_* | grep "valid data hammingLoss" | awk -F'==' '{print $2}'
-  echo "its about time"
-  grep -w "SVD_K_NUM" -A 2 report/${f}${dataResource}_* | grep "time" > tmpr
-  grep -w "logistic regression done" -A 20 report/${f}${dataResource}_* | grep "time" >> tmpr 
-  awk -F'==' '{a[$1] = a[$1]" "$2;}END{for(i in a){ print i""a[i]; }}' tmpr | sort -t" " -k1,2 | awk -F' ' '{print $3" "$4" ~ "$5" "$6;}'
-  rm tmpr
+  echo "dataResource ${dataResource}"
+  for f in "100F" "200F" "300F" "400F"
+  do
+    echo ""
+    echo "$f"
+    echo "microF1"
+    grep "logistic regression done" -A ${line} report/${f}${dataResource}_* | grep "valid data microF1" | awk -F'==' '{print $2}'
+    echo "one"
+    grep "logistic regression done" -A ${line} report/${f}${dataResource}_* | grep "valid data oneError" | awk -F'==' '{print $2}'
+    echo "rl"
+    grep "logistic regression done" -A ${line} report/${f}${dataResource}_* | grep "valid data RL" | awk -F'==' '{print $2}'
+    echo "coverage"
+    grep "logistic regression done" -A ${line} report/${f}${dataResource}_* | grep "valid data coverage" | awk -F'==' '{print $2}'
+    echo "avg Prec"
+    grep "logistic regression done" -A ${line} report/${f}${dataResource}_* | grep "valid data avgPrec" | awk -F'==' '{print $2}'
+    echo "hamming"
+    grep "logistic regression done" -A ${line} report/${f}${dataResource}_* | grep "valid data hammingLoss" | awk -F'==' '{print $2}'
+    echo "its about time"
+    grep -w "SVD_K_NUM" -A 2 report/${f}${dataResource}_* | grep "time" > tmpr
+    grep -w "logistic regression done" -A 20 report/${f}${dataResource}_* | grep "time" >> tmpr 
+    awk -F'==' '{a[$1] = a[$1]" "$2;}END{for(i in a){ print i""a[i]; }}' tmpr | sort -t" " -k1,2 | awk -F' ' '{print $3" "$4" ~ "$5" "$6;}'
+    rm tmpr
+  done
 done
--- iter.sh
+++ iter.sh
@@ -16,68 +16,71 @@ ind2Ratings[2]="data/yelp/yelp.data.filtered.int"
 ind2Attrs[2]="data/yelp/biz.cates.filtered.int"
 ind2AttrNum[2]=12
 
-ind2f[3]="ego-net"
-ind2Ratings[2]="data/ego-net/0.edges.u2u.app"
-ind2Attrs[2]="data/ego-net/0.circles.u2f.filtered"
-ind2AttrNum[2]=34
+ind2f[3]="ego-net0"
+ind2Ratings[3]="data/ego-net/0.edges.u2u.app"
+ind2Attrs[3]="data/ego-net/0.circles.u2f.filtered"
+ind2AttrNum[3]=34
 
-ind2f[4]="ego-net"
-ind2Ratings[2]="data/ego-net/107.edges.u2u.app"
-ind2Attrs[2]="data/ego-net/107.circles.u2f.filtered"
-ind2AttrNum[2]=18
+ind2f[4]="ego-net107"
+ind2Ratings[4]="data/ego-net/107.edges.u2u.app"
+ind2Attrs[4]="data/ego-net/107.circles.u2f.filtered"
+ind2AttrNum[4]=18
 
-ind2f[5]="ego-net"
-ind2Ratings[2]="data/ego-net/1684.edges.u2u.app"
-ind2Attrs[2]="data/ego-net/1684.circles.u2f.filtered"
-ind2AttrNum[2]=34
+ind2f[5]="ego-net1684"
+ind2Ratings[5]="data/ego-net/1684.edges.u2u.app"
+ind2Attrs[5]="data/ego-net/1684.circles.u2f.filtered"
+ind2AttrNum[5]=34
 
-ind2f[6]="ego-net"
-ind2Ratings[2]="data/ego-net/1912.edges.u2u.app"
-ind2Attrs[2]="data/ego-net/1912.circles.u2f.filtered"
-ind2AttrNum[2]=34
+ind2f[6]="ego-net1912"
+ind2Ratings[6]="data/ego-net/1912.edges.u2u.app"
+ind2Attrs[6]="data/ego-net/1912.circles.u2f.filtered"
+ind2AttrNum[6]=34
 
-ind2f[7]="ego-net"
-ind2Ratings[2]="data/ego-net/3437.edges.u2u.app"
-ind2Attrs[2]="data/ego-net/3437.circles.u2f.filtered"
-ind2AttrNum[2]=34
+ind2f[7]="ego-net3437"
+ind2Ratings[7]="data/ego-net/3437.edges.u2u.app"
+ind2Attrs[7]="data/ego-net/3437.circles.u2f.filtered"
+ind2AttrNum[7]=34
 
-ind2f[8]="ego-net"
-ind2Ratings[2]="data/ego-net/348.edges.u2u.app"
-ind2Attrs[2]="data/ego-net/348.circles.u2f.filtered"
-ind2AttrNum[2]=28
+ind2f[8]="ego-net348"
+ind2Ratings[8]="data/ego-net/348.edges.u2u.app"
+ind2Attrs[8]="data/ego-net/348.circles.u2f.filtered"
+ind2AttrNum[8]=28
 
-ind2f[9]="ego-net"
-ind2Ratings[2]="data/ego-net/3980.edges.u2u.app"
-ind2Attrs[2]="data/ego-net/3980.circles.u2f.filtered"
-ind2AttrNum[2]=34
+ind2f[9]="ego-net3980"
+ind2Ratings[9]="data/ego-net/3980.edges.u2u.app"
+ind2Attrs[9]="data/ego-net/3980.circles.u2f.filtered"
+ind2AttrNum[9]=34
 
-ind2f[10]="ego-net"
-ind2Ratings[2]="data/ego-net/414.edges.u2u.app"
-ind2Attrs[2]="data/ego-net/414.circles.u2f.filtered"
-ind2AttrNum[2]=14
+ind2f[10]="ego-net414"
+ind2Ratings[10]="data/ego-net/414.edges.u2u.app"
+ind2Attrs[10]="data/ego-net/414.circles.u2f.filtered"
+ind2AttrNum[10]=14
 
-ind2f[11]="ego-net"
-ind2Ratings[2]="data/ego-net/686.edges.u2u.app"
-ind2Attrs[2]="data/ego-net/686.circles.u2f.filtered"
-ind2AttrNum[2]=28
+ind2f[11]="ego-net686"
+ind2Ratings[11]="data/ego-net/686.edges.u2u.app"
+ind2Attrs[11]="data/ego-net/686.circles.u2f.filtered"
+ind2AttrNum[11]=28
 
-ind2f[12]="ego-net"
-ind2Ratings[2]="data/ego-net/698.edges.u2u.app"
-ind2Attrs[2]="data/ego-net/698.circles.u2f.filtered"
-ind2AttrNum[2]=26
+ind2f[12]="ego-net698"
+ind2Ratings[12]="data/ego-net/698.edges.u2u.app"
+ind2Attrs[12]="data/ego-net/698.circles.u2f.filtered"
+ind2AttrNum[12]=26
 
 
-for ind in 0 1 2                  # <-- 3 dataset
+for ind in {3..12}                  # <-- N dataset
 do
     f=${ind2f[${ind}]}
     for KNum in 100 200 300 400   # <-- 4 SVD_K_NUM
     do
-        for iter in 0 1 2 3       # vvv 5 iterations
+        for iterNum in {0..4}       # vvv 10 iterations
         do
+            echo "[info] Now f=${f} KNum=${KNum} iterNum==${iterNum}"
+            let iter="${iterNum}*2"
             LAMBDA=0.001 USR_TOTAL_LABELS_FIELDS=${ind2AttrNum[${ind}]} SVD_K_NUM=${KNum} MAX_TRAIN_NM=10000000 python2.7 -u run.py ${ind2Ratings[${ind}]} ${ind2Attrs[${ind}]} ${iter} > report/${KNum}F${f}_${iter} &
+            let iter2="${iterNum}*2+1"
+            LAMBDA=0.001 USR_TOTAL_LABELS_FIELDS=${ind2AttrNum[${ind}]} SVD_K_NUM=${KNum} MAX_TRAIN_NM=10000000 python2.7 -u run.py ${ind2Ratings[${ind}]} ${ind2Attrs[${ind}]} ${iter2} > report/${KNum}F${f}_${iter2}
         done
-        LAMBDA=0.001 USR_TOTAL_LABELS_FIELDS=${ind2AttrNum[${ind}]} SVD_K_NUM=${KNum} MAX_TRAIN_NM=10000000 python2.7 -u run.py ${ind2Ratings[${ind}]} ${ind2Attrs[${ind}]} 4 > report/${KNum}F${f}_4 
-                                  # ^^^ 5 iterations
+                                  # ^^^ 10 iterations
     done
 done
 
